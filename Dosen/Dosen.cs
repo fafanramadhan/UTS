0bf19e5e0a2d6e6e0a7d3396ca9991ab8eaa66ae
fafan ramadhan <forweare7@gmail.com>
2022-05-17 13:34:34 +0700

    Add project files.

---
+++ Dosen/Dosen.cs
@@ -0,0 +1,33 @@
+﻿internal class Dosen
+{
+    public int Id { get; set; }
+    public string Nama { get; set; }
+    public int Nik { get; set; }
+    public string Gender { get; set; }
+    public string Course { get; set; }
+    public string OtCourse { get; set; }
+
+
+    //method
+    public void DosenPengampu() 
+    {
+        Console.WriteLine("Nama Dosen   : Kamarudin, M.Kom");
+        Console.WriteLine("Mata Kuliah  :Pemrograman");        
+    }
+
+    public void InfoDosen()
+    {
+        Console.WriteLine("ID Dosen\t: {0}", Id);
+        Console.WriteLine("Nama Dosen\t: {0}", Nama);
+        Console.WriteLine("Nik\t\t: {0}", Nik);
+        Console.WriteLine("Gender\t\t: {0}", Gender);        
+        Console.WriteLine("Mata kuliah\t: {0}", Course + "," + OtCourse);
+    }
+    
+    public void Matkul()
+    {
+        
+        Console.WriteLine("Mata kuliah Lain: ");
+       OtCourse = Console.ReadLine();
+    }
+}
---
+++ Dosen/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Dosen dodef = new Dosen();
+dodef.DosenPengampu();
+
+Console.WriteLine();//space
+
+Console.WriteLine("\t Dosen ");
+
+Console.WriteLine();//space
+
+Dosen dos1 = new Dosen();
+Console.Write("Id\t\t: ");
+dos1.Id = int.Parse(Console.ReadLine()); // sama kek convert.toint32
+Console.Write("Nama Dosen\t: ");
+dos1.Nama = Console.ReadLine();
+Console.Write("NIK Dosen\t: ");
+dos1.Nik = int.Parse(Console.ReadLine());
+Console.Write("Gender\t\t: ");
+dos1.Gender = Console.ReadLine();
+Console.Write("Course\t\t: ");
+dos1.Course = Console.ReadLine();
+Console.ReadKey();
+
+Console.Clear();
+Console.WriteLine("\t Dosen ");
+dodef.DosenPengampu();
+Console.WriteLine();
+dos1.Matkul();
+Console.WriteLine();
+dos1.InfoDosen();
+
+
+
